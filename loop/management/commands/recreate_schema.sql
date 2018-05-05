4f200cf14b577cd77f1ca1f530f11ddebdf25308
Abhishek Lodha <alodha21051992@gmail.com>
2018-05-05 10:50:41 +0530

    merged master

---
+++ loop/management/commands/recreate_schema.sql
@@ -0,0 +1,31 @@
+DROP TABLE IF EXISTS `loop_aggregated_myisam`;
+
+CREATE TABLE `loop_aggregated_myisam`(
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `aggregator_id` int unsigned NOT NULL,
+  `date` date NOT NULL,
+  `mandi_id` int unsigned NOT NULL,
+  `gaddidar_id` int unsigned NOT NULL,
+  `quantity` decimal(10,3),
+  `amount` decimal(10,3),
+  `transportation_cost` decimal(10,3),
+  `farmer_share` decimal(10,3),
+  `gaddidar_share` decimal(10,3),
+  `aggregator_incentive` decimal(10,3),
+  `aggregator_name` varchar(255) NOT NULL,
+  `mandi_name` varchar(255) NOT NULL,
+  `gaddidar_name` varchar(255) NOT NULL,
+  `new_distinct_farmer` int unsigned NOT NULL,
+  `country_id` int unsigned NOT NULL,
+  `state_id` int unsigned NOT NULL,
+  `district_id` int unsigned NOT NULL,
+  `partner_id` int unsigned NOT NULL,
+  PRIMARY KEY(`id`)
+)ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 ;
+
+CREATE INDEX loop_aggregated_myisam_date ON loop_aggregated_myisam(date);
+CREATE INDEX loop_aggregated_myisam_date_aggregator_mandi ON loop_aggregated_myisam(date,aggregator_id,mandi_id);
+CREATE INDEX loop_aggregated_myisam_date_aggregator_mandi_gaddidar ON loop_aggregated_myisam(date,aggregator_id,mandi_id,gaddidar_id);
+CREATE INDEX loop_aggregated_myisam_date_country ON loop_aggregated_myisam(date,country_id);
+CREATE INDEX loop_aggregated_myisam_date_country_state ON loop_aggregated_myisam(date,country_id,state_id);
+CREATE INDEX loop_aggregated_myisam_date_country_state_district ON loop_aggregated_myisam(date,country_id,state_id,district_id);
